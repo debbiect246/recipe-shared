438e140e6c91445bde3dacd8e7cb7d85341eb7e1
Deborah Thompson <deb246@btinternet.com>
2023-09-20 14:20:15 +0100

    added user authentication created recipe app

--- main/settings.py
+++ main/settings.py
@@ -44,7 +44,12 @@ INSTALLED_APPS = [
 
     #local apps
     'home',
+    'users',
 
+    # 3rd party
+    'crispy_forms',
+    'crispy_bootstrap4',
+ 
 ]
 
 MIDDLEWARE = [
@@ -133,3 +138,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+#overide account url paths
+
+LOGIN_REDIRECT_URL = 'recipes-home'
+LOGIN_URL = 'user-login'
--- main/urls.py
+++ main/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 #finding urls for each app in project
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('register/', user_views.register, name="user-register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="user-login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="user-logout"),
+    path('profile/', user_views.profile, name="user-profile"),
 ]
